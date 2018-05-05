529083294396eebebb51940a5c69df2697a719e7
aadilahmed <aadilahmed0@gmail.com>
2018-05-04 22:12:00 -0700

    Added files

---
+++ app/src/main/java/com/example/android/popularmovies/utils/NetworkUtils.java
@@ -0,0 +1,21 @@
+package com.example.android.popularmovies.utils;
+
+import java.net.URL;
+
+public class NetworkUtils {
+    final static String MOVIE_DB_BASE_URL = "http://api.themoviedb.org/3/movie";
+    final static String PARAM_POPULAR = "popular";
+    final static String PARAM_RATING = "rating";
+
+    // PUT API KEY HERE
+    final static String API_KEY = "";
+
+    public URL buildURL(){
+        return null;
+    }
+
+    public static String getResponseFromHttpUrl(){
+        return null;
+    }
+
+}
