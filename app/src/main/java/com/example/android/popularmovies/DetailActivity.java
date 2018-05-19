14bfd1028d78873ddb42ddf7dd07d675a948c887
aadilahmed <aadilahmed0@gmail.com>
2018-05-19 15:53:00 -0700

    Improved detail UI

--- app/src/main/java/com/example/android/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies/DetailActivity.java
@@ -1,6 +1,5 @@
 package com.example.android.popularmovies;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -9,10 +8,10 @@ import android.widget.TextView;
 import com.example.android.popularmovies.model.Movie;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
 
 public class DetailActivity extends AppCompatActivity {
-    private static final String imageBase = "http://image.tmdb.org/t/p/original";
+    private static final String backdropBase = "http://image.tmdb.org/t/p/original";
+    private static final String posterBase = "http://image.tmdb.org/t/p/w185";
 
 
     @Override
@@ -20,21 +19,27 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView backdropImage = (ImageView) findViewById(R.id.backdrop_iv);
+        ImageView backdropImage = findViewById(R.id.backdrop_iv);
+        ImageView posterImage = findViewById(R.id.detail_poster_iv);
 
         TextView titleTv = findViewById(R.id.title_tv);
-        TextView plotSynopsisTv = (TextView) findViewById(R.id.plot_synopsis_tv);
-        TextView releaseDateTv = (TextView) findViewById(R.id.release_date_tv);
-        TextView userRatingTv = (TextView) findViewById(R.id.user_rating_tv);
+        TextView plotSynopsisTv = findViewById(R.id.plot_synopsis_tv);
+        TextView releaseDateTv = findViewById(R.id.release_date_tv);
+        TextView userRatingTv = findViewById(R.id.user_rating_tv);
 
         Bundle bundle = getIntent().getExtras();
         Movie movie = bundle.getParcelable("movie");
 
-        String imagePath = imageBase + movie.getBackdropPath();
+        String imagePath = backdropBase + movie.getBackdropPath();
+        String posterPath = posterBase + movie.getPosterPath();
 
         Picasso.with(this).load(imagePath).into(backdropImage);
 
         titleTv.setText(movie.getTitle());
         plotSynopsisTv.setText(movie.getOverview());
+        releaseDateTv.setText(movie.getReleaseDate());
+        userRatingTv.setText(Double.toString(movie.getVoteAverage()));
+
+        Picasso.with(this).load(posterPath).into(posterImage);
     }
 }
